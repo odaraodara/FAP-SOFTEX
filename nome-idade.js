b68e5c2adb8ca07d0017e8c6f2e6bad2001f0636
Odara Valença <104281379+odaraodara@users.noreply.github.com>
2023-07-30 12:35:32 +0000

    tratamento de erro

---
+++ nome-idade.js
@@ -0,0 +1,20 @@
+var readlineSync = require('readline-sync');
+
+let rodar = true;
+
+while (rodar) {
+    try {
+        let nome = readlineSync.question('Nome:');
+        let ano = readlineSync.questionInt('Ano de nascimento:');
+
+        if (ano > 1922 && ano < 2022) {
+            console.log("Nome:", nome);
+            console.log("Sua idade em 2023:", 2023 - ano);
+            rodar = false
+        }
+
+    } catch (error) {
+        console.log("Dados incorretos")
+
+    }
+}
