4cbde7eec3b8e8d2d3a238d7ff2e671ab450e29b
Odara Valença <104281379+odaraodara@users.noreply.github.com>
2023-08-04 19:24:00 +0000

    array

--- listas.js
+++ listas.js
@@ -1,10 +1,18 @@
+//criar três listas em JavaScript para armazenar nome, idade e cores favoritas de 10 pessoas.
+
 let nomes = ['Aurora','Bruna','Chico','Dalva','Elis','Felipe','Gil','Helen','Isa','José'];
 let idades = [28,19,34,58,79,17,38,28,46,54];
 let cores = ['verde','rosa','branco','vermelho','amarelo','laranja','cinza','marrom','preto','roxo'];
 
+//imprimir listas
+
 console.log(nomes,idades,cores);
+
+//modificar itens das listas
 
 idades[4] =80;
 cores[4] = 'amarelo-dourado';
+
+//imprimir listas novamente
 
 console.log(nomes,idades,cores);
