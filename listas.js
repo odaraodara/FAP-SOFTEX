01c1b43172db1147662123c3c4183d3e1548c429
Odara Valença <104281379+odaraodara@users.noreply.github.com>
2023-08-04 19:17:08 +0000

    array

---
+++ listas.js
@@ -0,0 +1,10 @@
+let nomes = ['Aurora','Bruna','Chico','Dalva','Elis','Felipe','Gil','Helen','Isa','José'];
+let idades = [28,19,34,58,79,17,38,28,46,54];
+let cores = ['verde','rosa','branco','vermelho','amarelo','laranja','cinza','marrom','preto','roxo'];
+
+console.log(nomes,idades,cores);
+
+idades[4] =80;
+cores[4] = 'amarelo-dourado';
+
+console.log(nomes,idades,cores);
