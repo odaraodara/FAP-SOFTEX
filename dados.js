dc556ca434b57caea4f0e51ccc2140ef7d10c654
Odara Valença <104281379+odaraodara@users.noreply.github.com>
2023-08-19 14:51:06 +0000

    dados primitivos

---
+++ dados.js
@@ -0,0 +1,21 @@
+let boolean;
+let bigInt;
+let string;
+let symbol;
+
+console.log(boolean,bigInt,string,symbol);
+
+boolean = null;
+bigInt = null;
+string = null;
+symbol = null;
+
+console.log(boolean,bigInt,string,symbol);
+
+boolean = true;
+bigInt = 192922838838484n;
+string = "isso é uma String";
+symbol = Symbol();
+
+console.log(`Boolean: ${boolean} / BigInt: ${bigInt} / String: ${string} / Symbol:`, symbol)
+
