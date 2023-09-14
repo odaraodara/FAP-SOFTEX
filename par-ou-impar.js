6cdaa1d49f6a39e050f80ee749ac6be2c4d39e7b
Odara Valença <odaravalenca@gmail.com>
2023-09-13 21:00:07 -0300

    par ou impar

---
+++ par-ou-impar.js
@@ -0,0 +1,10 @@
+function parOuImpar(num){
+    if(num %2 == 0){
+        console.log(`${num} é par!`)
+    }else{
+        console.log(`${num} é impar!`)
+    }
+}
+
+parOuImpar(3) // 3 é impar!
+parOuImpar(30) // 30 é par!
